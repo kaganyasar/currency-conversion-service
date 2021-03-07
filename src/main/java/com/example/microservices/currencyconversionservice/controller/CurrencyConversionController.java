d6b1e53e19d3574ea1d2a5eba180010fba93c28a
kaganyasar <o.kagan_yasar@hotmail.com>
2021-03-07 21:48:11 +0300

    "microservices"

--- src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.example.microservices.currencyconversionservice.controller;
 
 import com.example.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.example.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,9 @@ public class CurrencyConversionController {
 
     private static final RestTemplate REST_TEMPLATE = new RestTemplate();
 
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         Map<String, String> uriVariables = new HashMap<>();
@@ -28,7 +33,21 @@ public class CurrencyConversionController {
                     from, to, quantity,
                     currencyConversion.getConversionMultiple(),
                     quantity.multiply(currencyConversion.getConversionMultiple()),
-                    currencyConversion.getEnvironment()
+                    currencyConversion.getEnvironment() + " from rest template"
+            );
+        }
+        throw new RuntimeException("Unable to find data for " + from + " to " + to);
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+        if (currencyConversion != null) {
+            return new CurrencyConversion(currencyConversion.getId(),
+                    from, to, quantity,
+                    currencyConversion.getConversionMultiple(),
+                    quantity.multiply(currencyConversion.getConversionMultiple()),
+                    currencyConversion.getEnvironment() + " from feign"
             );
         }
         throw new RuntimeException("Unable to find data for " + from + " to " + to);

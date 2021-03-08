3c79a3f44d8edccbac57477899e262d49076aba6
kaganyasar <o.kagan_yasar@hotmail.com>
2021-03-09 00:14:52 +0300

    "microservices"

--- src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
